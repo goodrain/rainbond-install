7e762f2f0119e4597455823674b3e75fb3904aa0
zhengh <zhengh@goodrain.com>
2018-03-27 17:57:53 +0800

    [REV] update shell

--- install/pillar/system_info.sls
+++ install/pillar/system_info.sls
@@ -1,3 +1,10 @@
+hostname: manage01
 rbd-path: /opt/rainbond
+<<<<<<< Updated upstream
 inet-ip: 172.16.0.129
 role: manage
+=======
+db-user: USER
+db-pass: PASS
+inet-ip: 172.16.0.172
+>>>>>>> Stashed changes
--- scripts/check.sh
+++ scripts/check.sh
@@ -22,7 +22,7 @@ function Check_Internet(){
 function Set_Hostname(){
     hostname manage01
     echo "manage01" > /etc/hostname
-    Write_File add hostname "hostname: $DEFAULT_HOSTNAME"
+    Write_Sls_File  hostname "$DEFAULT_HOSTNAME"
 }
 
 
@@ -45,36 +45,8 @@ function Get_Rainbond_Install_Path(){
       install_path=$DEFAULT_INSTALL_PATH
       Echo_Info "Use default path:$DEFAULT_INSTALL_PATH"
   fi
-  Write_File add rbd-path "rbd-path: $install_path"
+  Write_Sls_File rbd-path "$install_path"
 }
-
-# Name   : Install_Salt
-# Args   : Null
-# Return : 0|!0
-function Install_Salt(){
-  ./scripts/bootstrap-salt.sh  -M -X -R $SALT_REPO  $SALT_VER 2>&1 > ${LOG_DIR}/${SALT_LOG}
-  # write salt config
-cat > /etc/salt/master.d/pillar.conf << END
-pillar_roots:
-  base:
-    - /_install_dir_/rainbond/install/pillar
-END
-cat > /etc/salt/master.d/salt.conf << END
-file_roots:
-  base:
-    - /_install_dir_/rainbond/install/salt
-END
-
-    rbd_path=$( grep rbd-path $INFO_FILE | awk '{print $2}')
-    sed "s/_install_dir_/${rbd-path}/g" /etc/salt/master.d/pillar.conf
-    sed "s/_install_dir_/${rbd-path}/g" /etc/salt/master.d/salt.conf
-    echo "master: $hostname" > /etc/salt/minion.d/minion.conf 
-    cp -rp ./install/pillar /${rbd-path}/rainbond/install/
-    cp -rp ./install/ /${rbd-path}/rainbond/install/
-    systemctl start salt-master
-    systemctl start salt-minion
-}
-
 
 # Name   : Check_System_Version
 # Args   : sys_name、sys_version
@@ -85,8 +57,8 @@ function Check_System_Version(){
        [[ "$sys_version" =~ "7" ]] \
     || [[ "$sys_version" =~ "9" ]] \
     || [[ "$sys_version" =~ "16.04" ]] \
-    || ( Write_File err_log "$sys_name$sys_version is not supported temporarily"
-        exit 1)
+    || #( Write_File err_log "$sys_name$sys_version is not supported temporarily"
+        exit 1
 }
 
 # Name   : Get_Net_Info
@@ -99,15 +71,15 @@ function Get_Net_Info(){
   if [ ! -z "$inet_ip" ];then
     for net_card in $net_cards
     do
-      grep "inet_ip" /etc/sysconfig/network-scripts/ifcfg-$net_card \
-      && Write_File add inet-ip "inet-ip: ${inet_ip}"
+      grep "$inet_ip" /etc/sysconfig/network-scripts/ifcfg-$net_card \
+      && Write_Sls_File inet-ip "${inet_ip}"
     done
   else
-        Write_File err_log "There is no inet ip, exit ..."
+        #Write_File err_log "There is no inet ip, exit ..."
     exit 1
   fi
   if [ ! -z "$public_ip" ];then
-    Write_File add public-ip "public-ip: ${public_ip}"
+    Write_Sls_File public-ip "${public_ip}"
   fi
 }
 
@@ -120,15 +92,16 @@ function Get_Hardware_Info(){
     cpu_num=$(grep "processor" /proc/cpuinfo | wc -l )
     memory_size=$(free -h | grep Mem | awk '{print $2}' | cut -d 'G' -f1 | awk -F '.' '{print $1}')
     if [ $cpu_num -lt 2 ];then
-      Write_File err_log "There is $cpu_num cpus, you need more cpu, exit ..."
+      #Write_File err_log "There is $cpu_num cpus, you need more cpu, exit ..."
       exit 1
     fi
     
     if [ $memory_size -lt 2 ];then
-      Write_File err_log "There is $memory_size G memories, you need more memories, exit ..."
+      #Write_File err_log "There is $memory_size G memories, you need more memories, exit ..."
       exit 1
     elif [ $memory_size -ge 2 -a $memory_size -lt 4 ];then
-      Write_File err_log "There is $memory_size G memories, It is less than the standard quantity(4 memories), It may affect system performance"
+      Echo_Info "There is $memory_size G memories, It is less than the standard quantity(4 memories), It may affect system performance"
+      #Write_File err_log "There is $memory_size G memories, It is less than the standard quantity(4 memories), It may affect system performance"
     fi
 }
 
@@ -138,13 +111,12 @@ function Get_Hardware_Info(){
 function Download_package(){
     curl -s $OSS_DOMAIN/$OSS_PATH/ctl.md5sum -o /tmp/ctl.md5sum
     curl -s $OSS_DOMAIN/$OSS_PATH/ctl.tgz -o /tmp/ctl.tgz
-    cd /tmp
     md5sum -c /tmp/ctl.md5sum > /dev/null 2>&1
     if [ $? -eq 0 ];then
       tar xf /tmp/ctl.tgz -C /usr/local/bin/ --strip-components=1
       return 0
     else
-      Write_File err_log "The packge is broken, please contact staff to repair."
+      # Write_File err_log "The packge is broken, please contact staff to repair."
       exit 1
     fi
     rm /tmp/ctl.* -rf
@@ -154,8 +126,35 @@ function Download_package(){
 # Args   : db_name、db_pass
 # Return : 0|!0
 function Write_Config(){
-    Write_File db-user "${DB-USER}"
-    Write_File db-pass "${DB-PASS}"
+    Write_Sls_File db-user "${DB-USER}"
+    Write_Sls_File db-pass "${DB-PASS}"
+}
+
+# Name   : Install_Salt
+# Args   : Null
+# Return : 0|!0
+function Install_Salt(){
+  ./scripts/bootstrap-salt.sh  -M -X -R $SALT_REPO  $SALT_VER 2>&1 > ${LOG_DIR}/${SALT_LOG}
+  # write salt config
+cat > /etc/salt/master.d/pillar.conf << END
+pillar_roots:
+  base:
+    - _install_dir_/rainbond/install/pillar
+END
+cat > /etc/salt/master.d/salt.conf << END
+file_roots:
+  base:
+    - _install_dir_/rainbond/install/salt
+END
+    rbd_path=$(grep rbd-path $INFO_FILE | awk '{print $2}')
+    sed -i "s#_install_dir_#${rbd_path}#g" /etc/salt/master.d/pillar.conf
+    sed -i "s#_install_dir_#${rbd_path}#g" /etc/salt/master.d/salt.conf
+    echo "master: $hostname" > /etc/salt/minion.d/minion.conf 
+    [ -d ${rbd_path}/rainbond/install/ ] || mkdir -p ${rbd_path}/rainbond/install/
+    cp -rp ./install/pillar ${rbd_path}/rainbond/install/
+    cp -rp ./install/ ${rbd_path}/rainbond/install/
+    systemctl start salt-master
+    systemctl start salt-minion
 }
 
 
@@ -165,26 +164,26 @@ function Write_Config(){
 function Write_Sls_File(){
   key=$1
   value=$2
-  if grep $key $INFO_FILE ;then
+  grep $key $INFO_FILE > /dev/null
+  if [ $? -eq 0 ];then
     sed -i -e "/$key/d" $INFO_FILE
-  else
-    echo "$key: $value" >> $INFO_FILE
   fi
+    echo "$key: $value" >> $INFO_FILE
 }
 
 #=============== main ==============
 
 Echo_Info "Checking internet connect ..."
-Check_Internet $RAINBOND_HOMEPAGE && Echo_Ok || Echo_Error
+#Check_Internet $RAINBOND_HOMEPAGE && Echo_Ok || Echo_Error
 
 Echo_Info "Setting [ manage01 ] for hostname ..."
-Set_Hostname && Echo_Ok || Echo_Error
+#Set_Hostname && Echo_Ok || Echo_Error
 
 Echo_Info "Configing installation path ..."
-Get_Rainbond_Install_Path  && Echo_Ok || Echo_Error
+#Get_Rainbond_Install_Path  && Echo_Ok || Echo_Error
 
 Echo_Info "Checking system version ..."
-Check_System_Version && Echo_Ok || Echo_Error
+#Check_System_Version && Echo_Ok || Echo_Error
 
 #ipaddr(inet pub) type .mark in .sls
 Echo_Info "Getting net information ..."
@@ -192,14 +191,14 @@ Get_Net_Info && Echo_Ok || Echo_Error
 
 # disk cpu memory
 Echo_Info "Getting Hardware information ..."
-Get_Hardware_Info && Echo_Ok || Echo_Error 
+#Get_Hardware_Info && Echo_Ok || Echo_Error 
 
 Echo_Info "Downloading Components ..."
-Download_package && Echo_Ok || Echo_Error
+#Download_package && Echo_Ok || Echo_Error
 
 Echo_Info "Writing configuration ..."
-Write_Config && Echo_Ok || Echo_Error
+#Write_Config && Echo_Ok || Echo_Error
 
 
 Echo_Info "Installing salt ..."
-Install_Salt && Echo_Ok || Echo_Error
+#Install_Salt && Echo_Ok || Echo_Error
