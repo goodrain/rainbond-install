45ce0f5ab0a3947da40ff0fad898e7cf2ebe8567
zhouyq <zhouyq@goodrain.com>
2018-03-30 17:26:30 +0800

    [FIX] fix -h args

--- setup.sh
+++ setup.sh
@@ -10,25 +10,27 @@ clear
 
 while getopts "i:fh" arg #选项后面的冒号表示该选项需要参数
 do
-        case $arg in
-             i)
-                export RBD_PATH="$OPTARG" #参数存在$OPTARG中
-                ;;
-             f)
-                export IS_FORCE="true"
-                ;;
-             r)
-                export RBD_ROLE="$OPTARG"
-                ;;
-             h)
-                Echo_Info "-f,          Ignore the hardware limit, Force install"
-                Echo_Info "-i [PATH],   Specify a installation path"
-                ;;
-             ?) #未识别的arg
-            echo "unkonw argument"
+    case $arg in
+    i)
+        export RBD_PATH="$OPTARG" #参数存在$OPTARG中
+        ;;
+    f)
+        export IS_FORCE="true"
+        ;;
+    r)
+        export RBD_ROLE="$OPTARG"
+        ;;
+    h)
+        Echo_Info "-f,          Ignore the hardware limit, Force install"
+        Echo_Info "-i [PATH],   Specify a installation path"
+        exit 0
+        ;;
+    ?) #未识别的arg
+        echo "unkonw argument"
         exit 1
         ;;
-        esac
+    esac
 done
-    /bin/bash ./scripts/check.sh
-    /bin/bash ./scripts/init-config.sh
+
+/bin/bash ./scripts/check.sh
+/bin/bash ./scripts/init-config.sh
