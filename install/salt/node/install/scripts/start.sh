e6421095d471d377f03cc998db3c20125f87dcf3
ysicing <root@ysicing.net>
2018-04-02 17:59:34 +0800

    [FIX] add forward.conf #20; update docker ulimit value #19

--- install/salt/node/install/scripts/start.sh
+++ install/salt/node/install/scripts/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 {% if "manage" in grains['host'] %}
-    eval $(ssh-agent) > /dev/null
-    eval $(ssh-add) > /dev/null
-    NODE_OPTS="--log-level=debug --kube-conf={{ pillar['rbd-path'] }}/kubernetes/kubecfg/admin.kubeconfig  --statsd.mapping-config={{ pillar['rbd-path'] }}/node/config/mapper.yml --static-task-path=/node/tasks/ --etcd=http://127.0.0.1:2379   --hostIP={{ pillar['inet-ip'] }} --run-mode master --noderule manage"
+eval $(ssh-agent) > /dev/null
+eval $(ssh-add) > /dev/null
+NODE_OPTS="--log-level=debug --kube-conf={{ pillar['rbd-path'] }}/kubernetes/kubecfg/admin.kubeconfig  --statsd.mapping-config={{ pillar['rbd-path'] }}/node/config/mapper.yml --static-task-path=/node/tasks/ --etcd=http://127.0.0.1:2379   --hostIP={{ pillar['inet-ip'] }} --run-mode master --noderule manage,compute"
 {% else %}
-    NODE_OPTS="--log-level=debug --kube-conf={{ pillar['rbd-path'] }}/kubernetes/kubecfg/admin.kubeconfig --statsd.mapping-config={{ pillar['rbd-path'] }}/node/config/mapper.yml --static-task-path=/node/tasks/ --etcd=http://127.0.0.1:2379  --hostIP={{ pillar['inet-ip'] }}"
+NODE_OPTS="--log-level=debug --kube-conf={{ pillar['rbd-path'] }}/kubernetes/kubecfg/admin.kubeconfig --statsd.mapping-config={{ pillar['rbd-path'] }}/node/config/mapper.yml --static-task-path=/node/tasks/ --etcd=http://127.0.0.1:2379  --hostIP={{ pillar['inet-ip'] }}"
 {% endif %}
 exec /usr/local/bin/node $NODE_OPTS
