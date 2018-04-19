c07c6fa5ec02452c193c6f012fe4557bf04496b9
zhouyq <zhouyq@goodrain.com>
2018-04-19 11:59:08 +0800

    [FIX] fix path error

--- scripts/common.sh
+++ scripts/common.sh
@@ -220,15 +220,15 @@ local l1=" ^" \
 }
 
 REG_Check(){
-    uid=$( Read_Sls_File reg-uuid ./install/pillar/goodrain.sls )
-    iip=$( Read_Sls_File inet-ip ./install/pillar/goodrain.sls )
+    uid=$( Read_Sls_File reg-uuid ./install/pillar/ )
+    iip=$( Read_Sls_File inet-ip ./install/pillar/ )
     curl --connect-timeout 20 ${RBD_DING}/chk\?uuid=$uid\&ip=$iip
 }
 
 REG_Status(){
-    uid=$( Read_Sls_File reg-uuid ./install/pillar/goodrain.sls )
-    iip=$( Read_Sls_File inet-ip ./install/pillar/goodrain.sls )
-    domain=$( Read_Sls_File domain /srv/pillar/goodrain.sls )
+    uid=$( Read_Sls_File reg-uuid ./install/pillar/ )
+    iip=$( Read_Sls_File inet-ip ./install/pillar/ )
+    domain=$( Read_Sls_File domain /srv/pillar/ )
     curl --connect-timeout 20 ${RBD_DING}/install\?uuid=$uid\&ip=$iip\&status=1\&domain=$domain
 }
 
