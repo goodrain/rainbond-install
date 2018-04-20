f02341a11242bcf945e22642d5d04299175da132
ysicing <root@ysicing.net>
2018-04-20 16:58:35 +0800

    [REV] update docker.sh #53

--- install/salt/plugins/data/init.sh
+++ install/salt/plugins/data/init.sh
@@ -3,7 +3,12 @@
 docker cp /tmp/init.sql rbd-db:/root
 docker exec rbd-db mysql -e "use console;source /root/init.sql;"
 
+{% if pillar['public-ip'] %}
+IP={{ pillar['public-ip'] }}
+{% else %}
 IP={{ pillar['inet-ip'] }}
+{% endif %}
+
 DOMAIN={{ pillar['domain'] }}
 cat > /tmp/region_info.sql <<EOF
 INSERT INTO \`region_info\` ( \`region_id\`, \`region_name\`, \`region_alias\`, \`url\`, \`token\`, \`status\`, \`desc\`, \`wsurl\`, \`httpdomain\`, \`tcpdomain\`) VALUES('asdasdasdasdasdasdasdasdas', 'cloudbang', '私有数据中心1', 'http://region.goodrain.me:8888', NULL, '1', '当前数据中心是默认安装添加的数据中心', 'ws://$IP:6060', '$DOMAIN', '$IP');
