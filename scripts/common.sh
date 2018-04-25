8e55a26627a511f59fe2a6ed0801a1c28cc7582c
ysicing <root@ysicing.net>
2018-04-25 14:54:10 +0800

    [REV] fixed typo #70

--- scripts/common.sh
+++ scripts/common.sh
@@ -70,8 +70,8 @@ CPU_LIMIT=2
 MEM_SIZE=$(free -h | grep Mem | awk '{print $2}' | cut -d 'G' -f1 | awk -F '.' '{print $1}')
 MEM_LIMIT=4
 
-DEFAULT_LOCAL_IP="$(ip ad | grep 'inet ' | egrep '10.|172.|192.168' | awk '{print $2}' | cut -d '/' -f 1 | grep -v '172.30.42.1' | head -1)"
-DEFAULT_PUBLIC_IP="$(ip ad | grep 'inet ' | egrep -v '10.|172.|192.168|127.' | awk '{print $2}' | cut -d '/' -f 1 | head -1)"
+DEFAULT_LOCAL_IP="$(ip ad | grep 'inet ' | egrep ' 10.|172.|192.168' | awk '{print $2}' | cut -d '/' -f 1 | grep -v '172.30.42.1' | head -1)"
+DEFAULT_PUBLIC_IP="$(ip ad | grep 'inet ' | egrep -v ' 10.|172.|192.168|127.' | awk '{print $2}' | cut -d '/' -f 1 | head -1)"
 DNS_SERVER="114.114.114.114"
 INIT_FILE="./.initialized"
 
