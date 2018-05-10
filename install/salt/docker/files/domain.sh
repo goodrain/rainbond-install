465a106c0763c73af7d2c731d26739eba2427c6a
ysicing <root@ysicing.net>
2018-05-10 13:56:20 +0800

    [FMT] update domain.sh

--- install/salt/docker/files/domain.sh
+++ install/salt/docker/files/domain.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 grep "domain" /srv/pillar/custom.sls
 if [[ $? -ne 0 ]];then
+    echo " " >> /srv/pillar/custom.sls
     echo "domain: $(cat {{ pillar['rbd-path'] }}/.domain.log)" >> /srv/pillar/custom.sls
 else
     echo "domain exist"
