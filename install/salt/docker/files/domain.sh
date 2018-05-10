1392b3a14d8828c904f57c23865fef2e3ed526ac
ysicing <root@ysicing.net>
2018-05-10 10:14:58 +0800

    [FIX] fix custom configuration errors.

--- install/salt/docker/files/domain.sh
+++ install/salt/docker/files/domain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-grep "domain" /srv/pillar/goodrain.sls
+grep "domain" /srv/pillar/custom.sls
 if [[ $? -ne 0 ]];then
-    echo "domain: $(cat {{ pillar['rbd-path'] }}/.domain.log)" >> /srv/pillar/goodrain.sls
+    echo "domain: $(cat {{ pillar['rbd-path'] }}/.domain.log)" >> /srv/pillar/custom.sls
 else
     echo "domain exist"
 fi
