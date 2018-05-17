1b8d9ee07dc1ef85bdbf02ab33e90082ce2d9b0e
ysicing <root@ysicing.net>
2018-05-17 18:02:43 +0800

    [REV] update domain.sh

--- install/salt/docker/files/domain.sh
+++ install/salt/docker/files/domain.sh
@@ -3,7 +3,7 @@
 DOMAIN_IP=$1
 DOMAIN_UUID={{ grains['uuid'] }}
 DOMAIN_LOG={{ pillar['rbd-path'] }}/.domain.log
-DOMAIN_API="http://domain-api.grapps.cn/domain"
+DOMAIN_API="http://domain.grapps.cn/domain/"
 
 if [ -f "/tmp/.goodrain" ];then
     DOMAIN_TYPE="True"
@@ -13,7 +13,7 @@ fi
 
 grep "domain" /srv/pillar/custom.sls
 if [[ $? -ne 0 ]];then
-    curl $DOMAIN_API?ip=$DOMAIN_IP\&uuid=$DOMAIN_UUID\&type=$DOMAIN_TYPE > $DOMAIN_LOG
+    curl -d 'ip=$DOMAIN_IP&uuid=$DOMAIN_UUID&type=$DOMAIN_TYPE&auth=' -X POST  $DOMAIN_API/new > $DOMAIN_LOG
     echo "domain: $(cat {{ pillar['rbd-path'] }}/.domain.log)" >> /srv/pillar/custom.sls
 else
     echo "domain exist"
