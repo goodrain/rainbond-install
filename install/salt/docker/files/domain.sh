befa92534c1943a8cd75334415b64ea04438f3be
ysicing <root@ysicing.net>
2018-05-18 12:38:01 +0800

    [FIX] update generate domain func

--- install/salt/docker/files/domain.sh
+++ install/salt/docker/files/domain.sh
@@ -3,7 +3,7 @@
 DOMAIN_IP=$1
 DOMAIN_UUID={{ grains['uuid'] }}
 DOMAIN_LOG={{ pillar['rbd-path'] }}/.domain.log
-DOMAIN_API="http://domain.grapps.cn/domain/"
+DOMAIN_API="http://domain.grapps.cn/domain"
 
 if [ -f "/tmp/.goodrain" ];then
     DOMAIN_TYPE="True"
@@ -13,8 +13,15 @@ fi
 
 grep "domain" /srv/pillar/custom.sls
 if [[ $? -ne 0 ]];then
-    curl -d 'ip=$DOMAIN_IP&uuid=$DOMAIN_UUID&type=$DOMAIN_TYPE&auth=' -X POST  $DOMAIN_API/new > $DOMAIN_LOG
-    echo "domain: $(cat {{ pillar['rbd-path'] }}/.domain.log)" >> /srv/pillar/custom.sls
+    curl -d 'ip='"$DOMAIN_IP"'&uuid='"$DOMAIN_UUID"'&type='"$DOMAIN_TYPE"'&auth='"$AUTH"'' -X POST  $DOMAIN_API/new > $DOMAIN_LOG
+    echo " " >> /srv/pillar/custom.sls
+    grep "grapps.cn" /opt/rainbond/.domain.log >/dev/null 
+    if [[ "$?" -eq 0 ]];then
+        echo "domain: $(cat /opt/rainbond/.domain.log)" >> /srv/pillar/custom.sls
+    else
+        echo "not generate, will use example"
+        echo "domain: www.example.com" >> /srv/pillar/custom.sls
+    fi
 else
     echo "domain exist"
 fi
