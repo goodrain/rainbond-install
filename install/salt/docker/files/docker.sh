5fa0e48fba5d986f624fdc20fc21d42e90c3fb49
ysicing <root@ysicing.net>
2018-06-08 11:38:52 +0800

    [REV] fix typo

--- install/salt/docker/files/docker.sh
+++ install/salt/docker/files/docker.sh
@@ -1,5 +1,5 @@
 {% if grains['os_family']|lower == 'redhat' %}
-DOCKER_OPTS="-H unix:///var/run/docker.sock --bip=172.30.42.1/16 --dns={{pillar['master-private-ip']}} --insecure-registry goodrain.me --storage-driver=devicemapper --storage-opt dm.fs=ext4 --userland-proxy=false"
-{% else %}
 DOCKER_OPTS="-H unix:///var/run/docker.sock --bip=172.30.42.1/16 --dns={{pillar['master-private-ip']}} --insecure-registry goodrain.me --storage-driver=devicemapper --storage-opt dm.xfs_nospace_max_retries=0 --userland-proxy=false"
+{% else %}
+DOCKER_OPTS="-H unix:///var/run/docker.sock --bip=172.30.42.1/16 --dns={{pillar['master-private-ip']}} --insecure-registry goodrain.me --storage-driver=devicemapper --userland-proxy=false"
 {% endif %}
