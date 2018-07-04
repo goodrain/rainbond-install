2b431bca2472527d9ec1c4b727be364f08bd690a
ysicing <root@ysicing.net>
2018-07-04 12:30:58 +0800

    fmt info

--- setup.sh
+++ setup.sh
@@ -79,9 +79,11 @@ install_func(){
         private_ip=$(yq r /srv/pillar/rainbond.sls master-private-ip)
         
         if [ ! -z "$public_ip" ];then
-            Echo_Banner "http://${public_ip}:7070/#/user/register"
+            Echo_Info "Please visit: http://${public_ip}:7070/#/user/register"
+            Echo_Banner "http://${public_ip}:7070"
         else
-            Echo_Banner "http://${private_ip}:7070/#/user/register"
+            Echo_Info "Please visit: http://${private_ip}:7070/#/user/register"
+            Echo_Banner "http://${private_ip}:7070"
         fi
     fi
 }
