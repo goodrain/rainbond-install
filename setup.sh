9178ea5fb02b44e18654921fa5642dd02041587c
ysicing <root@ysicing.net>
2018-07-04 12:02:38 +0800

    fmt install

--- setup.sh
+++ setup.sh
@@ -46,20 +46,23 @@ init_config(){
 
 install_func(){
     fail_num=0
+    step_num=1
+    all_steps=$(echo ${MANAGE_MODULES} | tr ' ' '\n' | wc -l)
     Echo_Info "will install manage node.It will take 15-30 minutes to install"
   
     for module in ${MANAGE_MODULES}
     do
         if [ "$module" = "plugins" -o "$module" = "proxy" ];then
-            Echo_Info "Start install $module, it will take 3-8 minutes "
+            Echo_Info "Start install step $module($step_num/$all_steps), it will take 3-8 minutes "
         else
-            Echo_Info "Start install $module ..."
+            Echo_Info "Start install step $module($step_num/$all_steps) ..."
         fi
         if ! (salt "*" state.sls $module);then
             ((fail_num+=1))
             break
         fi
-        sleep 2
+        ((step_num++))
+        sleep 1
     done
 
     if [ "$fail_num" -eq 0 ];then
@@ -74,10 +77,11 @@ install_func(){
         Echo_Info "install successfully"
         public_ip=$(yq r /srv/pillar/rainbond.sls master-public-ip)
         private_ip=$(yq r /srv/pillar/rainbond.sls master-private-ip)
+        
         if [ ! -z "$public_ip" ];then
-            Echo_Banner "http://${public_ip}:7070"
+            Echo_Banner "http://${public_ip}:7070/#/user/register"
         else
-            Echo_Banner "http://${private_ip}:7070"
+            Echo_Banner "http://${private_ip}:7070/#/user/register"
         fi
     fi
 }
