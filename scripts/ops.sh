af921228aff989275af328ba4aa513d9fb72e2ce
ysicing <root@ysicing.net>
2018-07-17 11:50:31 +0800

    fix  typo

--- scripts/ops.sh
+++ scripts/ops.sh
@@ -1,5 +1,7 @@
 [ -f "/etc/docker/daemon.json" ] && rm -rf /etc/docker/daemon.json
 systemctl restart docker
+systemctl restart salt-master
+systemctl restart salt-minion
 
 MANAGE_MODULES="misc \
 grbase \
@@ -13,34 +15,30 @@ proxy \
 kubernetes.node"
 
 install_func(){
-    fail_num=0
-    step_num=1
-    all_steps=$(echo ${MANAGE_MODULES} | tr ' ' '\n' | wc -l)
     echo "will install manage node.It will take 15-30 minutes to install"
-  
     for module in ${MANAGE_MODULES}
     do
         if [ "$module" = "plugins" -o "$module" = "proxy" ];then
-            echo "Start install $module(step: $step_num/$all_steps), it will take 3-8 minutes "
+            echo "Start install $module, it will take 3-8 minutes "
         else
-            echo "Start install $module(step: $step_num/$all_steps) ..."
+            echo "Start install $module ..."
         fi
         if ! (salt "*" state.sls $module);then
-            ((fail_num+=1))
             break
         fi
-        ((step_num++))
         sleep 1
     done
 
-    if [ "$fail_num" -eq 0 ];then
+    
+}
+
+install_func
+
+if [ "$?" -eq 0 ];then
         uuid=$(salt '*' grains.get uuid | grep "-" | awk '{print $1}')
         notready=$(grctl  node list | grep $uuid | grep false)
         if [ "$notready" != "" ];then
             grctl node up $uuid
         fi
-        echo "install successfully"
-    fi
-}
-
-install_func
+    echo "install successfully"
+fi
