b836cec151a5340d13abe1f2e8339050e22d35ff
ysicing <root@ysicing.net>
2018-07-30 17:13:41 +0800

    [REV]add docker health endpoint

--- install/salt/install/files/docker/docker.sh
+++ install/salt/install/files/docker/docker.sh
@@ -1,5 +1,5 @@
 {% if grains['os_family']|lower == 'redhat' %}
-DOCKER_OPTS="-H unix:///var/run/docker.sock --bip=172.30.42.1/16 --dns={{pillar['master-private-ip']}} --insecure-registry goodrain.me --storage-driver=devicemapper --storage-opt dm.xfs_nospace_max_retries=0 --userland-proxy=false"
+DOCKER_OPTS="-H unix:///var/run/docker.sock -H 127.0.0.1:2376 --bip=172.30.42.1/16 --dns={{pillar['master-private-ip']}} --insecure-registry goodrain.me --storage-driver=devicemapper --storage-opt dm.xfs_nospace_max_retries=0 --userland-proxy=false"
 {% else %}
-DOCKER_OPTS="-H unix:///var/run/docker.sock --bip=172.30.42.1/16 --dns={{pillar['master-private-ip']}} --insecure-registry goodrain.me --userland-proxy=false"
+DOCKER_OPTS="-H unix:///var/run/docker.sock -H 127.0.0.1:2376 --bip=172.30.42.1/16 --dns={{pillar['master-private-ip']}} --insecure-registry goodrain.me --userland-proxy=false"
 {% endif %}
