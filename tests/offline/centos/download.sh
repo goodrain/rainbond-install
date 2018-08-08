36312c70e214e504f9b66b05cc3274e55789e439
ysicing <root@ysicing.net>
2018-08-08 16:54:31 +0800

    [FMT] fix offline pkgs

--- tests/offline/centos/download.sh
+++ tests/offline/centos/download.sh
@@ -4,6 +4,7 @@ if [ "$1" == "bash" ];then
     exec /bin/bash
 else
     YAML_PATH=/opt/rainbond/install/rainbond.yaml.default
+    PKG_PATH=/opt/rainbond/install/install/pkgs
     cpkg=$(yq r $YAML_PATH rbd-pkgs.centos | awk '{print $2}')
     common_pkg=$(yq r $YAML_PATH rbd-pkgs.common | awk '{print $2}')
     for pkg in ${cpkg[@]} ${common_pkg[@]}
