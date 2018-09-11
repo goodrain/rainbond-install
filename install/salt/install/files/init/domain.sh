f06cfda3845734d652b49503047ef01a1ed7dff9
ysicing <root@ysicing.net>
2018-09-11 10:04:17 +0800

    [REV]update install type

--- install/salt/install/files/init/domain.sh
+++ install/salt/install/files/init/domain.sh
@@ -6,10 +6,13 @@ DOMAIN_LOG={{ pillar['rbd-path'] }}/.domain.log
 DOMAIN_API="http://domain.grapps.cn/domain"
 AUTH={{ pillar['secretkey'] }}
 DOMAIN_TYPE=False
-DOMAIN_TYPE={{ pillar['install-type'] }}
+INSTALL_TYPE={{ pillar['install-type'] }}
 
-if [[ "$DOMAIN_TYPE" != "offline" ]];then
+if [[ "$INSTALL_TYPE" != "offline" ]];then
     curl --connect-timeout 20  -d 'ip='"$DOMAIN_IP"'&uuid='"$DOMAIN_UUID"'&type='"$DOMAIN_TYPE"'&auth='"$AUTH"'' -X POST  $DOMAIN_API/new > $DOMAIN_LOG
+    cat > /tmp/.lock.domain <<EOF
+curl --connect-timeout 20  -d 'ip='"$DOMAIN_IP"'&uuid='"$DOMAIN_UUID"'&type='"$DOMAIN_TYPE"'&auth='"$AUTH"'' -X POST  $DOMAIN_API/new > $DOMAIN_LOG
+EOF
 fi
 
 [ -f $DOMAIN_LOG ] && wilddomain=$(cat $DOMAIN_LOG )
