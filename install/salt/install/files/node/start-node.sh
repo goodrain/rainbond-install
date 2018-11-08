6e4ff2aebd9dfb2b90613dc600ee9cc7f95be484
ysicing <ops.ysicing@gmail.com>
2018-11-08 16:50:14 +0800

    [REV] Adjust  node log level
    Adjust node log level

--- install/salt/install/files/node/start-node.sh
+++ install/salt/install/files/node/start-node.sh
@@ -2,11 +2,11 @@
 
 {% if "manage" in grains['id'] %}
 {% if grains['node_role'][0] == "worker" %}
-NODE_OPTS="--log-level=debug --kube-conf={{ pillar['rbd-path'] }}/etc/kubernetes/kubecfg/admin.kubeconfig --nodeid-file={{ pillar['rbd-path'] }}/etc/rbd-node/node_host_uuid.conf --static-task-path={{ pillar['rbd-path'] }}/etc/rbd-node/tasks/ --etcd=http://127.0.0.1:2379   --hostIP={{ grains['mip'][0] }} --service-list-file={{ pillar['rbd-path'] }}/conf/master.yaml --run-mode master --noderule manage,compute"
+NODE_OPTS="--log-level=warn  --kube-conf={{ pillar['rbd-path'] }}/etc/kubernetes/kubecfg/admin.kubeconfig --nodeid-file={{ pillar['rbd-path'] }}/etc/rbd-node/node_host_uuid.conf --static-task-path={{ pillar['rbd-path'] }}/etc/rbd-node/tasks/ --etcd=http://127.0.0.1:2379   --hostIP={{ grains['mip'][0] }} --service-list-file={{ pillar['rbd-path'] }}/conf/master.yaml --run-mode master --noderule manage,compute"
 {% else %}
-NODE_OPTS="--log-level=debug --kube-conf={{ pillar['rbd-path'] }}/etc/kubernetes/kubecfg/admin.kubeconfig --nodeid-file={{ pillar['rbd-path'] }}/etc/rbd-node/node_host_uuid.conf --static-task-path={{ pillar['rbd-path'] }}/etc/rbd-node/tasks/ --etcd=http://127.0.0.1:2379   --hostIP={{ grains['mip'][0] }} --service-list-file={{ pillar['rbd-path'] }}/conf/master.yaml --run-mode master --noderule manage"
+NODE_OPTS="--log-level=warn --kube-conf={{ pillar['rbd-path'] }}/etc/kubernetes/kubecfg/admin.kubeconfig --nodeid-file={{ pillar['rbd-path'] }}/etc/rbd-node/node_host_uuid.conf --static-task-path={{ pillar['rbd-path'] }}/etc/rbd-node/tasks/ --etcd=http://127.0.0.1:2379   --hostIP={{ grains['mip'][0] }} --service-list-file={{ pillar['rbd-path'] }}/conf/master.yaml --run-mode master --noderule manage"
 {% endif %}
 {% else %}
-NODE_OPTS="--log-level=debug --kube-conf={{ pillar['rbd-path'] }}/etc/kubernetes/kubecfg/admin.kubeconfig --nodeid-file={{ pillar['rbd-path'] }}/etc/rbd-node/node_host_uuid.conf --static-task-path={{ pillar['rbd-path'] }}/etc/rbd-node/tasks/ --etcd=http://127.0.0.1:2379  --hostIP={{ grains['mip'][0] }} --service-list-file={{ pillar['rbd-path'] }}/conf/worker.yaml"
+NODE_OPTS="--log-level=warn --kube-conf={{ pillar['rbd-path'] }}/etc/kubernetes/kubecfg/admin.kubeconfig --nodeid-file={{ pillar['rbd-path'] }}/etc/rbd-node/node_host_uuid.conf --static-task-path={{ pillar['rbd-path'] }}/etc/rbd-node/tasks/ --etcd=http://127.0.0.1:2379  --hostIP={{ grains['mip'][0] }} --service-list-file={{ pillar['rbd-path'] }}/conf/worker.yaml"
 {% endif %}
 exec /usr/local/bin/node $NODE_OPTS
