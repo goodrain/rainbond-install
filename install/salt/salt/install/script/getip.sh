c127b78601cff114893ea9a78b5469006bd50821
ysicing zheng <ysbot@ysicing.net>
2018-11-22 14:11:29 +0800

    fix salt mip

--- install/salt/salt/install/script/getip.sh
+++ install/salt/salt/install/script/getip.sh
@@ -2,7 +2,7 @@
 [ -f "/tmp/.role" ] && role="worker" || cat /etc/hostname | grep compute && role="worker" || role="master"
 
 host=$(hostname)
-ip=$(cat /tmp/salt-minion-uuid | grep "hostip" | awk -F+ '{print $2}')
+#ip=$(cat /tmp/salt-minion-uuid | grep "hostip" | awk -F+ '{print $2}')
 uuid=$(cat /tmp/salt-minion-uuid | grep "$ip+" | awk -F+ '{print $2}')
 [ -z "$ip" ] && ip=$(ip ad|grep inet|egrep ' 10.|172.|192.168'|awk '{print $2}'|cut -d '/' -f 1|grep -v '172.30.42.1'|head -1)
 if [ -z "$uuid" ];then
